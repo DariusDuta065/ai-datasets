1651948272839b018e810bbbfbe31e12d49a32d5
Darius Duta <dduta065@gmail.com>
2021-03-17 22:35:48 +0200

    Changed CTA link again

--- src/constants/Links.js
+++ src/constants/Links.js
@@ -1,5 +1,5 @@
 const Links = {
-  CONTACT_US: "https://jointwine.typeform.com/to/dBUDHHyG",
+  CONTACT_US: "https://jointwine.typeform.com/to/ZG2POq#ref=aidata",
 };
 
 export default Object.freeze(Links);
